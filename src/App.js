e9d100e5371436f2e8caa3204b14fcffcba0c3d1
Erick Yudi Fukuda Mifo <erickyudi99@gmail.com>
2020-12-23 14:02:29 -0300

    ADD
    +random jokes from the api

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import Header from './components/Header';
-import Joke from './components/Joke';
+import Joke from './components/Joke/random.js';
 import GlobalStyle from './styles/global';
 
 function App() {
--- src/components/Joke/random.js
+++ src/components/Joke/random.js
@@ -1,4 +1,6 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react';
+import instance from '../../axios/axios';
+
 import { Container } from './styles';
 import { makeStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
@@ -23,19 +25,35 @@ const useStyles = makeStyles({
 function Joke() {
     const classes = useStyles();
 
+
+    const[joke, setJoke] = useState('')
+    const[count, setCount] = useState(1)
+    
+
+    useEffect(() => {
+        async function getData() {
+            const request = await instance.get('/jokes/random')
+              setJoke(request.data.value.joke)
+        }
+      
+        getData()
+      
+    
+      }, [count])
+
   return (
     <Container>
-        <h2> Joke </h2>
+        <h2> Random Joke </h2>
         <Card className={classes.root}>
         <CardContent>
-            <Typography className={classes.title}>
-         TextoTexto TextoTexto TextoTexto  extoTexto TextoTexto TextoTexto TTextoTexto TextoTexto TextoTexto TextoTexto TextoTexto 
+        <Typography className={classes.title}>
+            {joke}
          </Typography>
         </CardContent>
         </Card>
 
-        <Button variant="contained" color="primary">
-        Primary
+        <Button variant="contained" color="primary" onClick={() => {setCount(count+1)}}>
+            Next Joke
         </Button>
 
     </Container>
