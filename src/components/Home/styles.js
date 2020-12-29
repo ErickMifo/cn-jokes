6e98bfe0d937c7dbe241a305a21be8faa510cdc6
Erick Yudi Fukuda Mifo <erickyudi99@gmail.com>
2020-12-29 13:34:59 -0300

    ADD
    +simple animation on homepage

--- src/components/Home/styles.js
+++ src/components/Home/styles.js
@@ -7,6 +7,18 @@ export const Container = styled.div`
     flex-direction: row;
     justify-content: space-evenly;
     margin-top: 12vh;
+    animation: buttonHomeAnimation 1s;
+
+    @keyframes buttonHomeAnimation {
+        from  {
+            opacity: 0;
+            margin-top: 20vh;
+        }
+        to    {
+            opacity: 1;
+            margin-top: 12vh;
+        }
+    }
 
     @media(max-width: 800px) {
         flex-direction: column;
@@ -24,7 +36,18 @@ export const DataContainer = styled.div`
     justify-content: space-evenly;
     align-items: center;
     margin-top: 12vh;
-    
+    animation: dataHomeAnimation 3s;
+
+    @keyframes dataHomeAnimation {
+        from {
+            opacity: 0;
+        }
+        to {
+            opacity: 1;
+        }
+    }
+
+
     @media(max-width: 425px) {
         margin-top: 4vh;
         padding: 16px;
